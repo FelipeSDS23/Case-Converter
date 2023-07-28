a48140e57385941ed2c7a392b297d62c055429ba
FelipeSDS23 <felipe03san@gmail.com>
2023-07-27 21:03:28 -0300

    Sentence case bug corrigido

--- js/script.js
+++ js/script.js
@@ -12,19 +12,20 @@ try {
             return;
         }
         if (el.hasAttribute("btnSentenceCapitalized")) {
-            const points = [".", "?", "!", "\n"];
-    
-            editedText = text.toLowerCase();
 
-            for (let i = 0; i < points.length; i++) {
-                editedText = sentenceCase(editedText, points[i]);
-            }
-            
+            const regex = /([\.!?\n])([\s]*)(\w)/g;
+    
+            let newText = text[0].toUpperCase() + text.substring(1, text.length).toLowerCase();
+
+            editedText = newText.replace(regex, function(a,b,c,d,e,f){
+                return `${b}${c}${d.toUpperCase()}`;
+            })
+        
         }
-        if (el.hasAttribute("btnFirstCapitalized")) {
-            editedText = text.toLowerCase();
-            editedText = editedText[0].toUpperCase() + editedText.substring(1, editedText.length);
-        }
+        // if (el.hasAttribute("btnFirstCapitalized")) {
+        //     editedText = text.toLowerCase();
+        //     editedText = editedText[0].toUpperCase() + editedText.substring(1, editedText.length);
+        // }
         if (el.hasAttribute("btnUppercase")) {
             editedText = text.toLowerCase();
         }
@@ -39,35 +40,6 @@ try {
         }
         textArea.value = editedText;
     })
-    
-    function sentenceCase(txt, dot) {
-        let fullTxt = txt;
-        let txtEdited = "";
-    
-        while (fullTxt.length) {
-            let sentence = fullTxt.substring(0, fullTxt.indexOf(dot) + 1)
-            if (!fullTxt.includes(dot)) {
-                sentence = fullTxt.substring(0, fullTxt.length);
-                fullTxt = "";
-            } else {
-                fullTxt = fullTxt.substring(fullTxt.indexOf(dot) + 1, fullTxt.length);
-            }
-            let i = 0;
-    
-            while (sentence[i] === " " || sentence[i] === "\n" || sentence[i] === dot) {
-                i++;
-            }
-    
-            if (sentence === "." || sentence === "?" || sentence === "!" || sentence === "\n") {
-                txtEdited += sentence;
-                continue;
-            }
-            
-            sentence = sentence.replace(sentence[i], sentence[i].toUpperCase())
-            txtEdited += sentence;
-        }
-        return txtEdited;
-    }
 
 } catch (e) {
     console.log(`Ocorreu um erro: ${e}`);
@@ -79,3 +51,88 @@ textArea.addEventListener("input", checkLength);
 function checkLength() {
     textLength.textContent = textArea.value.length;
 }
+
+
+
+
+// const textArea = document.getElementById("textarea");
+// const optionsList = document.querySelector("#options-list");
+// const textLength = document.querySelector("#text-length");
+
+// try {
+//     optionsList.addEventListener("click", function (e) {
+//         const el = e.target;
+//         const text = textArea.value;
+//         let editedText = "";
+    
+//         if (!text || el.hasAttribute("options")) {
+//             return;
+//         }
+//         if (el.hasAttribute("btnSentenceCapitalized")) {
+//             const points = [".", "?", "!", "\n"];
+    
+//             editedText = text.toLowerCase();
+
+//             for (let i = 0; i < points.length; i++) {
+//                 editedText = sentenceCase(editedText, points[i]);
+//             }
+            
+//         }
+//         if (el.hasAttribute("btnFirstCapitalized")) {
+//             editedText = text.toLowerCase();
+//             editedText = editedText[0].toUpperCase() + editedText.substring(1, editedText.length);
+//         }
+//         if (el.hasAttribute("btnUppercase")) {
+//             editedText = text.toLowerCase();
+//         }
+//         if (el.hasAttribute("btnLowercase")) {
+//             editedText = text.toUpperCase();
+//         }
+//         if (el.hasAttribute("btnAllCapitalized")) {
+//             subStr = text.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
+//                 return a.toUpperCase();
+//             })
+//             editedText = subStr;
+//         }
+//         textArea.value = editedText;
+//     })
+    
+//     function sentenceCase(txt, dot) {
+//         let fullTxt = txt;
+//         let txtEdited = "";
+    
+//         while (fullTxt.length) {
+//             let sentence = fullTxt.substring(0, fullTxt.indexOf(dot) + 1)
+//             if (!fullTxt.includes(dot)) {
+//                 sentence = fullTxt.substring(0, fullTxt.length);
+//                 fullTxt = "";
+//             } else {
+//                 fullTxt = fullTxt.substring(fullTxt.indexOf(dot) + 1, fullTxt.length);
+//             }
+//             let i = 0;
+    
+//             while (sentence[i] === " " || sentence[i] === "\n" || sentence[i] === dot) {
+//                 i++;
+//             }
+    
+//             if (sentence === "." || sentence === "?" || sentence === "!" || sentence === "\n") {
+//                 txtEdited += sentence;
+//                 continue;
+//             }
+            
+//             sentence = sentence.replace(sentence[i], sentence[i].toUpperCase())
+//             txtEdited += sentence;
+//         }
+//         return txtEdited;
+//     }
+
+// } catch (e) {
+//     console.log(`Ocorreu um erro: ${e}`);
+// }
+
+
+// textArea.addEventListener("input", checkLength);
+
+// function checkLength() {
+//     textLength.textContent = textArea.value.length;
+// }
